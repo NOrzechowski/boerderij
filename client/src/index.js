378c2417d715f59037d4a082b537a20ce94d8a88
Neil Orzechowski <nvorzech@gmail.com>
2022-10-31 14:48:43 +0800

    some file layout changes as a switch to phoenix is considered

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,12 @@
-import Phaser from "phaser";
-import Game from "./scenes/game"
+import Phaser from 'phaser'
+import Game from './scenes/game'
 
 const config = {
-    type: Phaser.AUTO,
-    parent: "boerderij",
-    width: 2000,
-    height: 780,
-    scene: [
-        Game
-    ]
-};
+  type: Phaser.WEBGL,
+  parent: 'boerderij',
+  width: 2000,
+  height: 780,
+  scene: [Game]
+}
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -1,8 +1,9 @@
-import io from 'socket.io-client'
+// import io from 'socket.io-client'
 import Card from '../helpers/card'
 import Dealer from '../helpers/dealer'
 import Zone from '../helpers/zone'
 import Deck from '../helpers/deck'
+const { Socket } = require('phoenix-channels')
 
 export default class Game extends Phaser.Scene {
   constructor () {
@@ -14,85 +15,85 @@ export default class Game extends Phaser.Scene {
   preload () {
     //TODO: need to put the base path into an env variable
     //2
-    this.load.image('2_clubs', 'src/assets/cards/2_of_clubs.png')
-    this.load.image('2_diamonds', 'src/assets/cards/2_of_diamonds.png')
-    this.load.image('2_hearts', 'src/assets/cards/2_of_hearts.png')
-    this.load.image('2_spades', 'src/assets/cards/2_of_spades.png')
+    this.load.image('2_clubs', 'src/static/cards/2_of_clubs.png')
+    this.load.image('2_diamonds', 'src/static/cards/2_of_diamonds.png')
+    this.load.image('2_hearts', 'src/static/cards/2_of_hearts.png')
+    this.load.image('2_spades', 'src/static/cards/2_of_spades.png')
 
     //3
-    this.load.image('3_clubs', 'src/assets/cards/3_of_clubs.png')
-    this.load.image('3_diamonds', 'src/assets/cards/3_of_diamonds.png')
-    this.load.image('3_hearts', 'src/assets/cards/3_of_hearts.png')
-    this.load.image('3_spades', 'src/assets/cards/3_of_spades.png')
+    this.load.image('3_clubs', 'src/static/cards/3_of_clubs.png')
+    this.load.image('3_diamonds', 'src/static/cards/3_of_diamonds.png')
+    this.load.image('3_hearts', 'src/static/cards/3_of_hearts.png')
+    this.load.image('3_spades', 'src/static/cards/3_of_spades.png')
 
     //4
-    this.load.image('4_clubs', 'src/assets/cards/4_of_clubs.png')
-    this.load.image('4_diamonds', 'src/assets/cards/4_of_diamonds.png')
-    this.load.image('4_hearts', 'src/assets/cards/4_of_hearts.png')
-    this.load.image('4_spades', 'src/assets/cards/4_of_spades.png')
+    this.load.image('4_clubs', 'src/static/cards/4_of_clubs.png')
+    this.load.image('4_diamonds', 'src/static/cards/4_of_diamonds.png')
+    this.load.image('4_hearts', 'src/static/cards/4_of_hearts.png')
+    this.load.image('4_spades', 'src/static/cards/4_of_spades.png')
 
     //5
-    this.load.image('5_clubs', 'src/assets/cards/5_of_clubs.png')
-    this.load.image('5_diamonds', 'src/assets/cards/5_of_diamonds.png')
-    this.load.image('5_hearts', 'src/assets/cards/5_of_hearts.png')
-    this.load.image('5_spades', 'src/assets/cards/5_of_spades.png')
+    this.load.image('5_clubs', 'src/static/cards/5_of_clubs.png')
+    this.load.image('5_diamonds', 'src/static/cards/5_of_diamonds.png')
+    this.load.image('5_hearts', 'src/static/cards/5_of_hearts.png')
+    this.load.image('5_spades', 'src/static/cards/5_of_spades.png')
 
     //6
-    this.load.image('6_clubs', 'src/assets/cards/6_of_clubs.png')
-    this.load.image('6_diamonds', 'src/assets/cards/6_of_diamonds.png')
-    this.load.image('6_hearts', 'src/assets/cards/6_of_hearts.png')
-    this.load.image('6_spades', 'src/assets/cards/6_of_spades.png')
+    this.load.image('6_clubs', 'src/static/cards/6_of_clubs.png')
+    this.load.image('6_diamonds', 'src/static/cards/6_of_diamonds.png')
+    this.load.image('6_hearts', 'src/static/cards/6_of_hearts.png')
+    this.load.image('6_spades', 'src/static/cards/6_of_spades.png')
 
     //7
-    this.load.image('7_clubs', 'src/assets/cards/7_of_clubs.png')
-    this.load.image('7_diamonds', 'src/assets/cards/7_of_diamonds.png')
-    this.load.image('7_hearts', 'src/assets/cards/7_of_hearts.png')
-    this.load.image('7_spades', 'src/assets/cards/7_of_spades.png')
+    this.load.image('7_clubs', 'src/static/cards/7_of_clubs.png')
+    this.load.image('7_diamonds', 'src/static/cards/7_of_diamonds.png')
+    this.load.image('7_hearts', 'src/static/cards/7_of_hearts.png')
+    this.load.image('7_spades', 'src/static/cards/7_of_spades.png')
 
     //8
-    this.load.image('8_clubs', 'src/assets/cards/8_of_clubs.png')
-    this.load.image('8_diamonds', 'src/assets/cards/8_of_diamonds.png')
-    this.load.image('8_hearts', 'src/assets/cards/8_of_hearts.png')
-    this.load.image('8_spades', 'src/assets/cards/8_of_spades.png')
+    this.load.image('8_clubs', 'src/static/cards/8_of_clubs.png')
+    this.load.image('8_diamonds', 'src/static/cards/8_of_diamonds.png')
+    this.load.image('8_hearts', 'src/static/cards/8_of_hearts.png')
+    this.load.image('8_spades', 'src/static/cards/8_of_spades.png')
 
     //9
-    this.load.image('9_clubs', 'src/assets/cards/9_of_clubs.png')
-    this.load.image('9_diamonds', 'src/assets/cards/9_of_diamonds.png')
-    this.load.image('9_hearts', 'src/assets/cards/9_of_hearts.png')
-    this.load.image('9_spades', 'src/assets/cards/9_of_spades.png')
+    this.load.image('9_clubs', 'src/static/cards/9_of_clubs.png')
+    this.load.image('9_diamonds', 'src/static/cards/9_of_diamonds.png')
+    this.load.image('9_hearts', 'src/static/cards/9_of_hearts.png')
+    this.load.image('9_spades', 'src/static/cards/9_of_spades.png')
 
     //10
-    this.load.image('10_clubs', 'src/assets/cards/10_of_clubs.png')
-    this.load.image('10_diamonds', 'src/assets/cards/10_of_diamonds.png')
-    this.load.image('10_hearts', 'src/assets/cards/10_of_hearts.png')
-    this.load.image('10_spades', 'src/assets/cards/10_of_spades.png')
+    this.load.image('10_clubs', 'src/static/cards/10_of_clubs.png')
+    this.load.image('10_diamonds', 'src/static/cards/10_of_diamonds.png')
+    this.load.image('10_hearts', 'src/static/cards/10_of_hearts.png')
+    this.load.image('10_spades', 'src/static/cards/10_of_spades.png')
 
     //jack
-    this.load.image('jack_clubs', 'src/assets/cards/jack_of_clubs.png')
-    this.load.image('jack_diamonds', 'src/assets/cards/jack_of_diamonds.png')
-    this.load.image('jack_hearts', 'src/assets/cards/jack_of_hearts.png')
-    this.load.image('jack_spades', 'src/assets/cards/jack_of_spades.png')
+    this.load.image('jack_clubs', 'src/static/cards/jack_of_clubs.png')
+    this.load.image('jack_diamonds', 'src/static/cards/jack_of_diamonds.png')
+    this.load.image('jack_hearts', 'src/static/cards/jack_of_hearts.png')
+    this.load.image('jack_spades', 'src/static/cards/jack_of_spades.png')
 
     //queen
-    this.load.image('queen_clubs', 'src/assets/cards/queen_of_clubs.png')
-    this.load.image('queen_diamonds', 'src/assets/cards/queen_of_diamonds.png')
-    this.load.image('queen_hearts', 'src/assets/cards/queen_of_hearts.png')
-    this.load.image('queen_spades', 'src/assets/cards/queen_of_spades.png')
+    this.load.image('queen_clubs', 'src/static/cards/queen_of_clubs.png')
+    this.load.image('queen_diamonds', 'src/static/cards/queen_of_diamonds.png')
+    this.load.image('queen_hearts', 'src/static/cards/queen_of_hearts.png')
+    this.load.image('queen_spades', 'src/static/cards/queen_of_spades.png')
 
     //king
-    this.load.image('king_clubs', 'src/assets/cards/king_of_clubs.png')
-    this.load.image('king_diamonds', 'src/assets/cards/king_of_diamonds.png')
-    this.load.image('king_hearts', 'src/assets/cards/king_of_hearts.png')
-    this.load.image('king_spades', 'src/assets/cards/king_of_spades.png')
+    this.load.image('king_clubs', 'src/static/cards/king_of_clubs.png')
+    this.load.image('king_diamonds', 'src/static/cards/king_of_diamonds.png')
+    this.load.image('king_hearts', 'src/static/cards/king_of_hearts.png')
+    this.load.image('king_spades', 'src/static/cards/king_of_spades.png')
 
     //ace
-    this.load.image('ace_clubs', 'src/assets/cards/ace_of_clubs.png')
-    this.load.image('ace_diamonds', 'src/assets/cards/ace_of_diamonds.png')
-    this.load.image('ace_hearts', 'src/assets/cards/ace_of_hearts.png')
-    this.load.image('ace_spades', 'src/assets/cards/ace_of_spades.png')
+    this.load.image('ace_clubs', 'src/static/cards/ace_of_clubs.png')
+    this.load.image('ace_diamonds', 'src/static/cards/ace_of_diamonds.png')
+    this.load.image('ace_hearts', 'src/static/cards/ace_of_hearts.png')
+    this.load.image('ace_spades', 'src/static/cards/ace_of_spades.png')
 
     // TODO: load remaining cards here
-    this.load.image('back', 'src/assets/card_back.png')
+    this.load.image('back', 'src/static/card_back.png')
   }
 
   create () {
@@ -110,27 +111,39 @@ export default class Game extends Phaser.Scene {
     let self = this
 
     // TODO: need to control this URL via env variable
-    this.socket = io('localhost:3000')
-    this.socket.on('connect', function () {
-      console.log('Connected!')
-    })
+    // this.socket = io('localhost:4000/socket')
+    // this.socket.on('connect', function () {
+    //   console.log('Connected!')
+    // })
 
-    this.socket.on('isPlayerA', function () {
+    this.socket = new Socket('ws:localhost:4000/socket')
+    this.socket.connect()
+    this.channel = this.socket.channel('moves', {})
+    console.log('try to join')
+    this.channel
+      .join()
+      .receive('ok', resp => {
+        console.log('Joined successfully', resp)
+      })
+      .receive('error', resp => {
+        console.log('Unable to join', resp)
+      })
+
+    console.log('channel: ', this.channel)
+
+    this.channel.on('moves:isPlayerA', function () {
       self.isPlayerA = true
     })
 
-    this.socket.on('dealCards', function () {
+    this.channel.on('moves:dealCards', function () {
       self.dealer.dealCards(self.deck)
       self.dealText.disableInteractive()
     })
 
-    this.socket.on('cardPlayed', function (
-      gameObject,
-      isPlayerA,
-      dropZone,
-      card
-    ) {
-      console.log(dropZone.data.values.cards)
+    this.channel.on('moves:cardPlayed', function (val) {
+      console.log('val yo: ', val)
+      const { gameObject, isPlayerA, dropZone, card } = val
+      console.log('dropzone: ', dropZone)
       //TODO: need improvements here
       if (isPlayerA !== self.isPlayerA) {
         let sprite = gameObject.textureKey
@@ -162,7 +175,7 @@ export default class Game extends Phaser.Scene {
       .setInteractive()
 
     this.dealText.on('pointerdown', function () {
-      self.socket.emit('dealCards')
+      self.channel.push('moves:dealCards')
     })
 
     this.dealText.on('pointerover', function () {
@@ -204,13 +217,12 @@ export default class Game extends Phaser.Scene {
         gameObject.x = dropZone.x + 0
         gameObject.y = dropZone.y + 350 + dropZone.data.values.cards.length * 30
         gameObject.disableInteractive()
-        self.socket.emit(
-          'cardPlayed',
-          gameObject,
-          self.isPlayerA,
-          dropZone,
-          sprite
-        )
+        self.channel.push('moves:cardPlayed', {
+          obj: gameObject,
+          isPlayerA: self.isPlayerA,
+          dropZone: dropZone,
+          sprite: sprite
+        })
       }
     })
   }
